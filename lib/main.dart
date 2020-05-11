44bd60f8cccc6d1619312ee045975e3650f139b4
Theran <theran.brigowatz@gmail.com>
2020-05-11 17:05:40 +0900

    practice layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue[300],
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.white,
+                child: Text("Container 1"),
+              ),
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(height: 100, width: 100, color: Colors.blue),
+                    Container(height: 100, width: 100, color: Colors.yellow),
+                  ]),
+              Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                  child: Text("Container 3")),
+            ],
+          ),
+        ),
       ),
     );
   }
