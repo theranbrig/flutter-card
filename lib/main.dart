758353d43f3bade8753ac2fb55927a3c08da8d53
Theran <theran.brigowatz@gmail.com>
2020-05-11 17:30:54 +0900

    fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/basic.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,29 +13,19 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue[300],
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.white,
-                child: Text("Container 1"),
-              ),
-              Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(height: 100, width: 100, color: Colors.blue),
-                    Container(height: 100, width: 100, color: Colors.yellow),
-                  ]),
-              Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                  child: Text("Container 3")),
-            ],
-          ),
+          child: Column(children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.blue[200],
+              backgroundImage: NetworkImage(
+                  'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'),
+            ),
+            Text(
+              "Baby Yoda",
+              style:
+                  GoogleFonts.lobster(color: Colors.indigo[900], fontSize: 40),
+            )
+          ]),
         ),
       ),
     );
